123786c69f3e7d81155aefcd2d3d90eb159aaca1
Flamur64 <Flamurim64@gmail.com>
2023-09-27 11:54:10 +0200

    commit

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
 import express from "express"
 import bodyParser from "body-parser"
-import fs from "fs"
-import path from "path"
 
 const app = express()
 const port = 3000
 
-app.get("/", (req, res) => {
-  res.send('<a href="/anmeldung">Zur Anmeldung</a>')
-})
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
+app.get("/", (req, res) => {
+    res.send('<div style="display: flex; justify-content: center; align-items: center; height: 100vh;"><a href="/anmeldung" style="text-decoration: none; background-color: #007bff; color: #fff; padding: 10px 20px; border-radius: 5px; font-weight: bold;">->Login zum Quizspiel<-</a></div>')
+  })
 
 app.get("/anmeldung", function (req, res) {
     res.sendFile("anmeldung.html", { root: "./" })
+  })
+  
+app.get("/quiz", function (req, res) {
+    res.sendFile("quiz.html", { root: "./" })
   })
 
   app.post("/anmeldung", function (req, res) {
     const { name, password } = req.body
     if (name === "gruppe1" && password === "12345") {
-      res.redirect("/tasks")
+      res.redirect("/quiz")
     } else {
       res.send("Anmeldung fehlgeschlagen.")
     }
